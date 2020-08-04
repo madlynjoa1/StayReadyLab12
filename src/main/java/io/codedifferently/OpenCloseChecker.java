32fd8369b990cf657e3d5add2aeb828016221029
madlynjoa1 <maddlynjoa1@icloud.com>
2020-08-04 10:49:17 -0400

    Ready to review

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,26 @@
 package io.codedifferently;
-
+import java.util.Stack;
 public class OpenCloseChecker {
+    public Boolean checkParenthesis(String input){
+        Stack<Character> stack = new Stack<>();
+        Boolean answer = false;
+        for(int index = 0; index < input.length(); index++){
+            if(input.charAt(index) == '(' || input.charAt(index) == '[' || input.charAt(index) == '{' || input.charAt(index) == '<'|| input.charAt(index) == '"'){
+                stack.push(input.charAt(index));
+            } else if (!stack.empty() && ((input.charAt(index) == ')' && stack.peek() == '(') ||
+                    (input.charAt(index) == ']' && stack.peek() == '[') ||
+                    (input.charAt(index) == '}') && stack.peek() == '{') ||
+                    (input.charAt(index) == '>' && stack.peek() == '<') ||
+                    (input.charAt(index) == '"' && stack.pop() == '"')) {
+                stack.pop();
+            }
+            else if (input.charAt(index) == ')' || input.charAt(index) == ']' || input.charAt(index) == '}' || input.charAt(index) == '>'|| input.charAt(index) == '"'){
+                return answer;
+            }
+        }
+        if(stack.empty()){
+            return answer = true;
+        }
+        else return answer;
+    }
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,77 @@
 package io.codedifferently;
-
+import org.junit.Assert;
+import org.junit.Test;
 public class OpenCloseCheckerTest {
-
+    @Test
+    public void checkParenthesisTest(){
+        // Given
+        OpenCloseChecker cParenthesis = new OpenCloseChecker();
+        // When
+        Boolean actual = cParenthesis.checkParenthesis("a(b)c(d)e(f)g");
+        // Then
+        Assert.assertTrue(actual);
+    }
+    @Test
+    public void checkParenthesisTest1(){
+        // Given
+        OpenCloseChecker cParenthesis = new OpenCloseChecker();
+        // When
+        Boolean actual = cParenthesis.checkParenthesis("a(bcd(ef))");
+        // Then
+        Assert.assertTrue(actual);
+    }
+    @Test
+    public void checkParenthesisTest2(){
+        // Given
+        OpenCloseChecker cParenthesis = new OpenCloseChecker();
+        // When
+        Boolean actual = cParenthesis.checkParenthesis("a(bc(d)))");
+        // Then
+        Assert.assertFalse(actual);
+    }
+    @Test
+    public void checkParenthesisTest3(){
+        // Given
+        OpenCloseChecker cParenthesis = new OpenCloseChecker();
+        // When
+        Boolean actual = cParenthesis.checkParenthesis(")");
+        // Then
+        Assert.assertFalse(actual);
+    }
+    @Test
+    public void checkParenthesisTest4(){
+        // Given
+        OpenCloseChecker cParenthesis = new OpenCloseChecker();
+        // When
+        Boolean actual = cParenthesis.checkParenthesis("()");
+        // Then
+        Assert.assertTrue(actual);
+    }
+    @Test
+    public void checkParenthesisTest5(){
+        // Given
+        OpenCloseChecker cParenthesis = new OpenCloseChecker();
+        // When
+        Boolean actual = cParenthesis.checkParenthesis("()(");
+        // Then
+        Assert.assertFalse(actual);
+    }
+    @Test
+    public void checkParenthesisTest6(){
+        // Given
+        OpenCloseChecker cParenthesis = new OpenCloseChecker();
+        // When
+        Boolean actual = cParenthesis.checkParenthesis("())");
+        // Then
+        Assert.assertFalse(actual);
+    }
+    @Test
+    public void checkParenthesisTest7(){
+        // Given
+        OpenCloseChecker cParenthesis = new OpenCloseChecker();
+        // When
+        Boolean actual = cParenthesis.checkParenthesis("([)])");
+        // Then
+        Assert.assertFalse(actual);
+    }
 }
